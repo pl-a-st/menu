9db38c9dba2c0febde5235d86c86e5d9121938f0
Максим Богащенко <sergeyivanov85@yandex.ru>
2022-10-19 19:47:56 +0300

    Добавьте файлы проекта.

---
+++ menu/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace menu {
+    public static class Menu<InputEnum> where InputEnum : Enum {
+        static private int SelectedPosition = 0;
+        static private Dictionary<InputEnum, string> Dictionary;
+        static private string MainTextMenu;
+        public static Enum GetSelectedInMenu(Dictionary<InputEnum, string> dictionary, string mainTextMenu) {
+            SelectedPosition = 0;
+            Dictionary = dictionary;
+            MainTextMenu = mainTextMenu;
+            GetStartedWithMenu();
+            return dictionary.ToList()[SelectedPosition].Key as System.Enum;
+            //return Enum.GetValues(typeof(InputEnum)).GetValue(SelectedPosition) as System.Enum; // так вытягиевает из Enum, но Enum может не соответствовать по порядку Dictionary
+        }
+        private static void GetStartedWithMenu() {
+            Console.CursorVisible = false;
+            StartMenu();
+            Console.CursorVisible = true;
+            Console.Clear();
+        }
+        private static void StartMenu() {
+            ConsoleKeyInfo key = default;
+            while (key.Key != ConsoleKey.Enter) {
+                Console.Clear();
+                Console.WriteLine(MainTextMenu);
+                RewriteMenuFromDictionary();
+                do {
+                    key = Console.ReadKey(true);
+                }
+                while (key.Key != ConsoleKey.UpArrow && key.Key != ConsoleKey.DownArrow && key.Key != ConsoleKey.Enter);
+                DefineSelectedPosition(key);
+            }
+        }
+        private static void DefineSelectedPosition(ConsoleKeyInfo key) {
+            if (key.Key == ConsoleKey.UpArrow && SelectedPosition > 0) {
+                SelectedPosition--;
+            }
+            if (key.Key == ConsoleKey.DownArrow && SelectedPosition < (Dictionary.Count - 1)) {
+                SelectedPosition++;
+            }
+        }
+        private static void RewriteMenuFromDictionary() {
+            int i = 0;
+            foreach (var row in Dictionary) {
+                if (i == SelectedPosition) {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine(row.Value);
+                Console.ResetColor();
+                i++;
+            }
+        }
+        public static Dictionary<InputEnum, string> GetTypicalDictionaryForMenu(string repeatingString) {
+            Dictionary<InputEnum, string> dictionary = new Dictionary<InputEnum, string>();
+            int i = 0;
+            foreach (string str in Enum.GetNames(typeof(InputEnum))) {
+                var inpuEnumExempl = (InputEnum)Enum.Parse(typeof(InputEnum), str, true);
+                dictionary.Add(inpuEnumExempl, repeatingString + " " + (i + 1));
+                i++;
+            }
+            return dictionary;
+        }
+    }
+}
---
+++ menu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace menu {
+    enum Tasks {
+        Task1,
+        Task2,
+        Task3,
+        Task4,
+        Task5,
+        Task6
+
+    }
+    class Program {
+        enum Lessons {
+            Lesson1,
+            Lesson2,
+            Lesson3,
+            Lesson4,
+            ExempleLesson
+        }
+        
+        enum Direction {
+            dB_mV,
+            mV_dB
+        }
+        static void Main(string[] args) {
+            var lesson = Menu<Lessons>.GetSelectedInMenu(Menu<Lessons>.GetTypicalDictionaryForMenu("Урок"), "Выберите урок: ");
+            var task = Menu<Tasks>.GetSelectedInMenu(Menu<Tasks>.GetTypicalDictionaryForMenu("Задание"), "Выберите задание: ");
+            var directionDictionary = new Dictionary<Direction, string>() {
+                {Direction.dB_mV,"дБ в мВ"},
+                {Direction.mV_dB,"мВ в dB"}
+            };
+            var direction = Menu<Direction>.GetSelectedInMenu(directionDictionary, "Выберите направление преобразования: ");
+        }
+      
+    }
+}
